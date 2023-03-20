d138e925619d1d407a8acf5c0dfb81d3ca55d916
soyedarif <soyedariff@gmail.com>
2023-03-20 19:18:09 +0600

    player toggle done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,45 @@
-'use strict';
+"use strict";
 
 //selecting elements
-const score0El=document.getElementById('score--0');
-const score1El=document.getElementById('score--1');
-const diceEl=document.querySelector('.dice');
-const btnNew=document.querySelector('.btn--new')
-const btnRoll=document.querySelector('.btn--roll')
-const btnHold=document.querySelector('.btn--hold')
+const player0El=document.querySelector('.player--0')
+const player1El=document.querySelector('.player--1')
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 //starting condition
-score0El.textContent=0
-score1El.textContent=0
-diceEl.classList.add('hidden')
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 //rolling dice functionality
-btnRoll.addEventListener('click',function(){
-    //1. Generating a random dice roll
-    const dice=Math.trunc(Math.random()*6)+1;
-    console.log(dice);
-    //2. Display Dice  
-    diceEl.classList.remove('hidden')
-    diceEl.src=`dice-${dice}.png`
-    //3. Check for rolled 1: if true, switch to next player
-    
-})
+btnRoll.addEventListener("click", function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2. Display Dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  //3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore=0;
+    activePlayer=activePlayer===0?1:0;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+  }
+});
